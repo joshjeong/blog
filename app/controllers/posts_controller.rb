1cb83287768d625ebf5d117181a5ecf687f38365
Josh Jeong <jcjeong@gmail.com>
2014-08-07 09:27:28 -0700

    Refactor and comments made

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,33 @@
 class PostsController < ApplicationController
 
   def index
-    @all_posts = Post.all
+    @posts = Post.all
   end
 
-  # def new
-  #   @post = Post.new
-  # end
+  def new #POST
+    @post = Post.new
+  end
 
-  def create
-    params.permit!
+  def create #GET
+    # params.permit!
     Post.create(params[:post])
-    redirect_to "/posts"
+    redirect_to :posts
   end
 
-  def show
+  def show #GET, shows page for indiv posts
     @post = Post.find(params[:id])
   end
 
-  def destroy
+  def destroy #DELETE
     Post.find(params[:id]).destroy
-    redirect_to "/posts"
+    redirect_to :posts
+  end
+
+  def update #PUT, updates db
+
+  end
+
+  def edit #GET, shows page to edit
   end
 
 end
