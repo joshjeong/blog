953d284c653b4ef47e7e2e5e31df7461dd5ae633
Josh Jeong <jcjeong@gmail.com>
2014-08-08 23:06:47 -0700

    add new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,21 @@ class CommentsController < ApplicationController
     @comments = @post.comments
   end
 
-  def show
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    params.permit!
+    @post = Post.find(params[:post_id])
+    @post.comments.create(params[:comment])
+    redirect_to :post_comments
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to :post_comments
   end
 
 end
